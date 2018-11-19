806f1cf3ded105cd9ce6ec8aeabdd745a8acbc89
Krzysztof Piechowicz <Krzysztof.Piechowicz@adesso.de>
2018-11-19 17:13:34 +0100

    read response status from correct variable

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const ping = ({ url, timeout, onlineResponses }) => {
     xhr.ontimeout = isOffline;
     xhr.onload = () => {
       const response = xhr.responseText.trim();
-      if (!response || !onlineResponses.includes(response.status)) {
+      if (!response || !onlineResponses.includes(xhr.status)) {
         isOffline();
       } else {
         isOnline();
